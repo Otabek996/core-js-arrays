1c848301b63cd41327f4ca904f3db8844a4cfed7
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-01-09 01:11:37 +0500

    feat: solve generate odds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -374,8 +374,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from(Array(len)).map((_, index) => index * 2 + 1);
 }
 
 /**
