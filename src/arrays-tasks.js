a82c0f832a13f8ac314659b22c5866cda4599724
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-01-09 01:58:20 +0500

    feat: solve get falsy values count

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -407,8 +407,9 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  if (arr.length === 0) return 0;
+  return arr.filter((element) => Boolean(element) === false).length;
 }
 
 /**
