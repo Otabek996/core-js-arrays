061a36a009ae2e7a5bd986ec3afa85efde421937
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-01-09 03:50:11 +0500

    feat: solve shift array

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -573,8 +573,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return arr
+    .slice(n !== -1 ? n + 1 : n + 2, arr.length)
+    .concat(arr.slice(0, n !== -1 ? n + 1 : n + 2));
 }
 
 /**
