57e40d0a3e47e6bfce284426b8569d5a1d80bbe4
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-01-08 23:21:51 +0500

    feat: solve get average

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -142,8 +142,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const average =
+    arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0) /
+    arr.length;
+  return Math.round(average * 100) / 100;
 }
 
 /**
